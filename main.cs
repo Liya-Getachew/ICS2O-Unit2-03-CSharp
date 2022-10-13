8ae420f1179c0648f6ac3d983b289575b56c1df4
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-13 18:18:52 +0000

    space

--- main.cs
+++ main.cs
@@ -7,7 +7,7 @@ using System;
 
 class Program
 {
-   public static void Main (string[] args)
+    public static void Main(string[] args)
     {
         // This function accepts user input
         int streetNumber;
