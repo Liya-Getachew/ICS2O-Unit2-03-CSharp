2a8dc902fa0ae3bee6e1c0dca3aca7409991e55a
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2022-10-13 18:07:16 +0000

    spacing

--- main.cs
+++ main.cs
@@ -8,22 +8,22 @@ using System;
 class Program 
 {
    public static void Main (string[] args) 
-   {
-       // This function accepts user input
-       int streetNumber;
-       string streetName;
-       
-       Console.WriteLine("This program gets a user's street number and name.");
-       Console.WriteLine("");
+    {
+        // This function accepts user input
+        int streetNumber;
+        string streetName;
+     
+        Console.WriteLine("This program gets a user's address.");
+        Console.WriteLine("");
 
-       Console.WriteLine("Enter your street number: ");
-       streetNumber = Convert.ToInt32(Console.ReadLine());
-       Console.WriteLine("Enter your street name: ");
-       streetName = Console.ReadLine();
+        Console.WriteLine("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter your street name: ");
+        streetName = Console.ReadLine();
 
-       Console.WriteLine("");
-       Console.WriteLine("Your street address is: " + streetNumber + " " + streetName + ".");
+        Console.WriteLine("");
+        Console.WriteLine("Your street address is: " + streetNumber + " " + streetName + ".");
 
-       Console.WriteLine("\nDone.");
-   }
+        Console.WriteLine("\nDone.");
+    }
 }
